dfc0617ce64d53e683714bb89d26162ed2c37c4e
Shakil Ashraful Anam <ashrafanam318@gmail.com>
2024-12-17 11:51:44 +0600

    logger

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Application } from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import connectDB from './config/db';
 import todoRoutes from './routes/todo';
@@ -8,6 +9,7 @@ import authRoutes from './routes/auth';
 const app: Application = express();
 const PORT: number = parseInt(process.env.PORT || '5000', 10);
 connectDB();
+app.use(morgan('dev'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
